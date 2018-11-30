f7ec87b1afcde25de1dbae1d2e266a9fa13fcdd8
pangluo <59036386@qq.com>
2018-11-30 13:11:47 +0800

    ``Change BasicAnnotation

--- Chapter/src/main/java/com/domoyun/testng/BasicAnnotation.java
+++ Chapter/src/main/java/com/domoyun/testng/BasicAnnotation.java
@@ -1,8 +1,15 @@
 package com.domoyun.testng;
 
+import org.testng.annotations.Test;
+
 /**
  * @author shkstart
  * @create 2018-11-30 13:02
  */
 public class BasicAnnotation {
+    //最基本注解，用来把方法标注为测试的一部分
+    @Test
+    public static void testCase1(){
+        System.out.println("这是一个测试用例1");
+    }
 }
