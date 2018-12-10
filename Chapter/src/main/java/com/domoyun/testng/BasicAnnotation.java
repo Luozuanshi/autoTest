62f6c5bd95aa0a3bd9f21b3ae3ba666a28cd3ae4
pangluo <59036386@qq.com>
2018-12-10 13:37:27 +0800

    ``

--- Chapter/src/main/java/com/domoyun/testng/BasicAnnotation.java
+++ Chapter/src/main/java/com/domoyun/testng/BasicAnnotation.java
@@ -8,7 +8,7 @@ import org.testng.annotations.*;
  */
 public class BasicAnnotation {
 
-    //最基本注解，用来把方法标注为测试的一部分
+    //最基本注解，用来把方法标注为测试的一部分`
     @Test
     public static void testCase1(){
         System.out.println("这是一个测试用例1");
