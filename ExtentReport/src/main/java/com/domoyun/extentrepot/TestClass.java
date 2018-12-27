37b0ad630a5bc96e9646914079ded7d44043f014
pangluo <59036386@qq.com>
2018-12-27 16:37:11 +0800

    New Module ExtentRepot Modify Module TestNG pom.xml

---
+++ ExtentReport/src/main/java/com/domoyun/extentrepot/TestClass.java
@@ -0,0 +1,29 @@
+package com.domoyun.extentrepot;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestClass {
+    @Test
+    public void test1() {
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void logDemo() {
+        Reporter.log("Reporter输出日志");
+        throw new RuntimeException("运行时异常");
+
+    }
+}
