1ddbd7aa6787bff29c790a30180379d161129152
pangluo <59036386@qq.com>
2019-01-19 23:58:20 +0800

    HttpClient first routine

---
+++ HttpClient/src/main/java/com/domoyun/httpclient/GetMethod.java
@@ -0,0 +1,21 @@
+package com.domoyun.httpclient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GetMethod {
+    @Test
+    public void test() throws IOException {
+        HttpGet get = new HttpGet("https://domoyun.com");
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        String result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
